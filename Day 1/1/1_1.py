35401f7a02b39147c7166abcd67b3c06452b6430
David Bradburn <david.bradburn2@gmail.com>
2024-12-04 21:01:07 +0000

    day 1 p1+p2 but a tad late

--- Day 1/1/1_1.py
+++ Day 1/1/1_1.py
@@ -2,7 +2,7 @@
 ###### https://adventofcode.com/2022/day/1 #######################
 #################################################################
 
-file = "test.txt"
+file = "input.txt"
 
 DAY_NO = "1"
 PART = "1"
@@ -11,3 +11,22 @@ file_path_base = "Day " + DAY_NO + "/misc/"
 
 with open(file_path_base + file, "r") as fd:
 	rawInput = fd.readlines()
+
+list_left = []
+list_right = []
+## I want to split into 2 lists
+
+for element in rawInput:
+	pair = element.strip("\n").split("   ")
+	list_left.append(int(pair[0]))
+	list_right.append(int(pair[1]))
+list_left.sort()
+list_right.sort()
+
+assert len(list_right) == len(list_left)
+
+total = 0
+for index in range(len(list_left)):
+	total += abs(list_left[index] - list_right[index])
+
+print(total)
--- Day 1/2/1_2.py
+++ Day 1/2/1_2.py
@@ -2,7 +2,7 @@
 ###### https://adventofcode.com/2022/day/1 #######################
 #################################################################
 
-file = "test.txt"
+file = "input.txt"
 
 DAY_NO = "1"
 PART = "2"
@@ -11,3 +11,29 @@ file_path_base = "Day " + DAY_NO + "/misc/"
 
 with open(file_path_base + file, "r") as fd:
 	rawInput = fd.readlines()
+
+list_left = []
+list_right = []
+## I want to split into 2 lists
+
+for element in rawInput:
+	pair = element.strip("\n").split("   ")
+	list_left.append(int(pair[0]))
+	list_right.append(int(pair[1]))
+list_left.sort()
+list_right.sort()
+
+assert len(list_right) == len(list_left)
+
+def find_count_in_list(num, arr):
+	count = 0
+	for element in arr:
+		if num == element:
+			count += 1
+	return count
+
+total = 0
+for ele in list_left:
+	total += ele * find_count_in_list(ele, list_right)
+
+print(total)
